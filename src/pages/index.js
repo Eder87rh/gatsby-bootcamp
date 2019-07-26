930ec5e6eb1692f1115d151d4565e5dbd200f105
Eder Ramirez <eder.ramirez87@gmail.com>
2019-07-26 12:33:55 -0500

    deleting "need a dev?" link

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
       <Head title="Home"/>
       <h1>Hello</h1>
       <h2>I'm Eder, A fulll-stack javascript developer living in Mexico</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </Layout>
   );
 };
